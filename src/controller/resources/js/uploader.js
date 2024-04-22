01942c554462ad1dfefe41224886bb9b422d5698
Amit Chatterjee <amit.chatterjee@donotwanttotell.io>
2024-04-21 21:08:37 -0400

    Enhanced the code to use fetch()

--- src/controller/resources/js/uploader.js
+++ src/controller/resources/js/uploader.js
@@ -16,10 +16,56 @@ class Uploader extends HTMLElement {
        }
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+
+        //this.resultFrame.hidden=false;
+
+        const form = event.currentTarget;
+        form.hidden=true;
+
+        const url = new URL(form.action);
+        const formData = new FormData(form);
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        const fetchOptions = {
+            method: form.method,
+            cache: "no-cache",
+            headers: {
+                "Accept": "application/json",
+            },
+            redirect: "follow",
+            body: formData,
+            signal
+        };
+      
+        const fetchPromise = fetch(url, fetchOptions);
+        fetchPromise.then(response => {
+            if (response.status == 200) {
+                response.json().then(json=> {
+                    console.log(json)
+                });
+            } else {
+                console.log("########## " + response.body);
+            }
+        }).catch(error => {
+            // Handle any errors that occurred during the fetch
+            console.error('Fetch error:', error);
+        }).finally(()=>{
+            clearTimeout(timeoutId); // Clear the timeout
+        });
+
+        const timeoutId = setTimeout(() => {
+            controller.abort(); // Abort the fetch request
+            console.log('Document submission timed out');
+        }, 5000);
+    }
+
     connectedCallback() {
         console.log('Conected to docriver-uploader for realm: ' + this.getAttribute("realm"));
         this.attachShadow({ mode: "open" }).innerHTML = `
-        <form method="POST" target="result" enctype="multipart/form-data">
+        <form method="POST" enctype="multipart/form-data">
         <label for="files">Files</label>
         <input type="file" id="files" name="files" required multiple>
         <input type="submit" value="Submit">
@@ -29,10 +75,14 @@ class Uploader extends HTMLElement {
         this.form = this.shadowRoot.querySelector("form");
         this.form.action = this.getAttribute('docServer') + "/tx/" + this.getAttribute("realm")
 
+        this.form.addEventListener('submit', this.handleSubmit);
+
+        /*
         this.form.addEventListener("submit", (e) => {
             this.form.hidden=true;
             this.resultFrame.hidden=false;
         });
+        */
 
         this.createHidden('refResourceType', this.getAttribute("refResourceType"));
         this.createHidden('refResourceId', this.getAttribute("refResourceId"));
