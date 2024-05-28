805854514be90372a9e5555ec56651dfb285c407
Amit Chatterjee <amit.chatterjee@donotwanttotell.io>
2024-05-28 07:57:43 -0400

    Adding keycloak SSO

---
+++ infrastructure/mysql/keycloak-init.sql
@@ -0,0 +1,4 @@
+CREATE USER 'keycloak'@'%' IDENTIFIED BY 'keycloak';
+CREATE DATABASE keycloak CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%';
+FLUSH PRIVILEGES;
